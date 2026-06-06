9e6a34d77787c247ec25fbe5a57d45002de2a8f4
waterprisem <103801679+waterprisem@users.noreply.github.com>
2026-06-06 18:45:34 -0400

    view project details buttons

--- js/script.js
+++ js/script.js
@@ -18,3 +18,41 @@ tabButtons.forEach(function(button) {
     document.querySelector(`.tab-content[data-tab="${tabName}"]`).classList.remove('hidden')
 })
 })
+
+const viewButtons = document.querySelectorAll('.view-btn')
+const overlay = document.querySelector('.modal-overlay')
+const modalTitle = document.querySelector('.modal-title')
+const modalDescription = document.querySelector('.modal-description')
+const modalImage = document.querySelector('.modal-image')
+const modalLink = document.querySelector('.modal-link')
+const closeButton = document.querySelector('.modal-close')
+
+
+
+viewButtons.forEach(function(button) {
+  button.addEventListener('click', function() {
+    const card = button.closest('.project-card')
+    const title = card.dataset.title
+    const description = card.dataset.description
+    const image = card.dataset.image
+    const link = card.dataset.link
+
+    modalTitle.textContent = title
+    modalDescription.textContent = description
+    modalImage.src = image
+    modalLink.href = link
+
+    overlay.classList.remove('hidden')
+    console.log('View button clicked:', title)
+  })
+})
+
+overlay.addEventListener('click', function(event) {
+    if (event.target === overlay){
+        overlay.classList.add('hidden')
+    }      
+})
+
+closeButton.addEventListener('click', function(event) {
+  overlay.classList.add('hidden')
+})
